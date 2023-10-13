0c9d4ab19c1edd83b310eda4060fe518f97e5705
Augusto Romero <augustoromerodev@gmail.com>
2023-10-13 17:37:44 -0300

    add agent template chatflow flowise

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwcXOaJFYF8ISoP4OmIs15xfHkTwitrsQ",
-  authDomain: "simple-todo-1a6c7.firebaseapp.com",
-  projectId: "simple-todo-1a6c7",
-  storageBucket: "simple-todo-1a6c7.appspot.com",
-  messagingSenderId: "369222179955",
-  appId: "1:369222179955:web:b5591f5075628118e91e06"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+console.log(process.env);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
 
 const db = getFirestore(app);
 export { db };
