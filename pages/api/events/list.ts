cf39b745ada6d7acb2c2df8f519f56ef82b92cea
Augusto Romero <augustoromerodev@gmail.com>
2023-11-02 21:23:30 -0300

    filter fix

--- pages/api/events/list.ts
+++ pages/api/events/list.ts
@@ -21,22 +21,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       switch (periodName) {
         case "week":
           startDate = startOfWeek(addDays(startOfToday(), 7 * periodNumber));
-          endDate = endOfWeek(addDays(startOfToday(), 7 * (periodNumber+1)));
+          endDate = endOfWeek(addDays(startOfToday(), 7 * (periodNumber)));
           break;
         case "weekend":
-          startDate = startOfWeek(addDays(startOfToday(),( 7 * (periodNumber+1))-1));
-          endDate = endOfWeek(addDays(startOfToday(), 7 * (periodNumber+1)));
+          startDate = startOfWeek(addDays(startOfToday(),( 7 * (periodNumber))-1));
+          endDate = endOfWeek(addDays(startOfToday(), 7 * (periodNumber)));
           endDate = addDays(endDate, -1);
           break;
         case "month":
           startDate = startOfMonth(addDays(startOfToday(), 30 * periodNumber));
-          endDate = endOfMonth(addDays(startOfToday(), 30 * (periodNumber+1)));
+          endDate = endOfMonth(addDays(startOfToday(), 30 * (periodNumber)));
+          endDate = addDays(endDate, -1);
+          break;
+        case "year":
+          startDate = new Date(new Date().getFullYear() + periodNumber, 0, 1);
+          endDate = new Date(new Date().getFullYear() + periodNumber, 11, 31);
           break;
         default:
           startDate = null;
           endDate = null;
           break;
       }
+      console.log(startDate, endDate)
       const eventsCollection = collection(db, "events");
       let q = startDate && endDate ? query(eventsCollection, where("date", ">=", startDate), where("date", "<=", endDate)) : eventsCollection;
       const eventsSnapshot = await getDocs(q);
